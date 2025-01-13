c605d221821ae0921975b0b87ddfe5b0d0e9eb2a
Tuan Luong <tdluong@crimson.ua.edu>
2025-01-13 15:06:43 -0600

    add bed track with CFAR

--- layer-tracker/ekf/cfar_1D.m
+++ layer-tracker/ekf/cfar_1D.m
@@ -1,7 +1,7 @@
 function [detections, threshold] = cfar_1D(data, guard_width, train_width, gain)
     pad = guard_width + train_width;
     threshold = data;
-    detections = [];
+    
     [peaks_data, peak_loc] = findpeaks(data);
     for ii = pad+1:size(data,1)- (pad + 1)
         leading_cell = data(ii-pad:ii-train_width);
@@ -16,7 +16,8 @@ function [detections, threshold] = cfar_1D(data, guard_width, train_width, gain)
     [peaks_data, peak_loc] = findpeaks(data);
     
     thres_at_peaks = threshold(peak_loc);
-    detections = peak_loc(peaks_data > thres_at_peaks);
-    detections = vertcat(detections, bed_loc);
+    detections = peak_loc(peaks_data > thres_at_peaks); % Get detection above threshold
+    detections(detections > bed_loc) = [];   % Remove detection below bed
+    detections = vertcat(detections, bed_loc); % Add bed
 
 end
--- layer-tracker/ekf/cfar_detector.m
+++ layer-tracker/ekf/cfar_detector.m
@@ -6,27 +6,39 @@ load ("../../raw-echogram/20181231_044516.mat");
 
 ascope_ind = 500;
 
-data  = detrend_data;
+data  = wiener2_modified;
+
+skip = 1;
 
 % CFAR parameters
-scaling_factor = 1.2;
-training_cell_width = 10;
-guard_cell_width = 10;
+scaling_factor = 1.1;
+training_cell_width = 5;
+guard_cell_width = 1;
 
 %% Pre allocate
 detection = cell(1, size(data,2));
 
-for kk = 1:size(data,2)
+for kk = 1:skip:size(data,2)
     [detection{1,kk}, ~] = cfar_1D(data(:,kk)+10, guard_cell_width, ...
         training_cell_width, scaling_factor); % Shift data up 100 to deal with negative
 end
 
+% [detection{1,ascope_ind}, threshold] = cfar_1D(data(:,ascope_ind)+50, guard_cell_width, ...
+%          training_cell_width, scaling_factor); % Shift data up 100 to deal with negative
+
+% x = ascope_ind * ones(size(detection{1,ascope_ind}));
 
 figure()
 imagesc(data)
 colormap (1-gray)
 hold on 
-for kk = 1:size(data,2)
+% scatter(x, detection{1,ascope_ind},'r.')
+for kk = 1:skip:size(data,2)
     x = kk * ones(size(detection{1,kk}));
     scatter(x, detection{1,kk},'r.')
 end
+
+% figure()
+% plot (data(:,ascope_ind)+50)
+% hold on
+% plot (threshold)
